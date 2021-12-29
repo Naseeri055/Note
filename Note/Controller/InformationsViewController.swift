eb7fad31a5234d600f5119ff2034eae68a96cdbb
Nasser Aseeri <naseeri1983@gmail.com>
2021-12-29 16:17:24 +0300

    first

---
+++ Note/Controller/InformationsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailsViewController.swift
+//  firebaseDemo
+//
+//  Created by user on 20/12/2021.
+//
+
+import UIKit
+
+class InformationsViewController: UIViewController {
+    var selectedPost:Post?
+    var selectedPostImage:UIImage?
+    @IBOutlet weak var postDescriptionLabel: UILabel!
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let selectedPost = selectedPost,
+        let selectedImage = selectedPostImage{
+            postTitleLabel.text = selectedPost.title
+            postDescriptionLabel.text = selectedPost.description
+            postImageView.image = selectedImage
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Note/Model/Post.swift
@@ -0,0 +1,31 @@
+//
+//  Post.swift
+//  firebaseDemo
+//
+//  Created by user on 20/12/2021.
+//
+
+import Foundation
+import Firebase
+struct Post {
+    var id = ""
+    var title = ""
+    var description = ""
+    var imageUrl = ""
+    var user:User
+    var createdAt:Timestamp?
+    
+    init(dict:[String:Any],id:String,user:User) {
+        if let title = dict["title"] as? String,
+           let description = dict["description"] as? String,
+           let imageUrl = dict["imageUrl"] as? String,
+            let createdAt = dict["createdAt"] as? Timestamp{
+            self.title = title
+            self.description = description
+            self.imageUrl = imageUrl
+            self.createdAt = createdAt
+        }
+        self.id = id
+        self.user = user
+    }
+}
